2d8e7c5d8fdfad9627daad00fffa7d453b1bf06f
Torsten Curdt <tcurdt@vafer.org>
2023-11-26 23:43:33 +0100

    trying caddy

---
+++ modules/caddy.nix
@@ -0,0 +1,18 @@
+{ config, pkgs, ... }:
+{
+
+  services.caddy = {
+    enable = true;
+
+    # curl localhost -i -H "Host: example.org"
+    virtualHosts."example.org" = {
+      extraConfig = ''
+        reverse_proxy http://127.0.0.1:8080
+        tls internal
+      '';
+      serverAlias = [ "old.example.org" ];
+  };
+
+}
+
+
