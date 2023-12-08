811ee214682919d8a7bde77ca8c27c1fd62527dc
Torsten Curdt <tcurdt@vafer.org>
2023-12-08 21:44:15 +0100

    hardware

--- hardware/utm.nix
+++ hardware/utm.nix
@@ -1,14 +1,19 @@
 { modulesPath, ... }:
 {
   imports = [ (modulesPath + "/profiles/qemu-guest.nix") ];
+
   boot.loader.grub = {
     efiSupport = true;
     efiInstallAsRemovable = true;
     device = "nodev";
   };
-  fileSystems."/boot" = { device = "/dev/disk/by-uuid/D155-033C"; fsType = "vfat"; };
+
   boot.initrd.availableKernelModules = [ "ata_piix" "uhci_hcd" "xen_blkfront" ];
   boot.initrd.kernelModules = [ "nvme" ];
+
+  fileSystems."/boot" = { device = "/dev/disk/by-uuid/D155-033C"; fsType = "vfat"; };
+
   fileSystems."/" = { device = "/dev/vda2"; fsType = "ext4"; };
+
   swapDevices = [ { device = "/dev/vda3"; } ];
 }
