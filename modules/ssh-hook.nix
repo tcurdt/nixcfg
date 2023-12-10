30eecb29ad656bd82d724ddc4e912d6d5343d0fa
Torsten Curdt <tcurdt@vafer.org>
2023-12-10 04:00:46 +0100

    quoting and cleanup

--- modules/ssh-hook.nix
+++ modules/ssh-hook.nix
@@ -2,67 +2,57 @@
 
 	let
 
-    deployScript = pkgs.writeScriptBin "deploy" ''
-      #!/usr/bin/env bash
-      echo "hello"
-      whoami
-      sudo whoami
-    '';
-
-    rebuildScript = pkgs.writeScriptBin "rebuild" ''
-      #!/usr/bin/env bash
-      echo "hello"
+    hookScript = pkgs.writeScriptBin "hook" ''
+      #!${pkgs.bash}/bin/bash
+      echo "hello $1"
+      echo "$out"
       whoami
     '';
 
-    # hookScript = pkgs.writeScriptBin "hook" ''
-    #   #!/usr/bin/env bash
-    #   set -eu
-    #   set -o pipefail
-    #   [[ -z "${SSH_ORIGINAL_COMMAND:-}" ]] && exit 1
-    #   case "${SSH_ORIGINAL_COMMAND}" in
-    #     "deploy"*)
-    #       exec <module>/bin/deploy
-    #       ;;
-    #     *)
-    #       echo "invalid command"
-    #       exit 1
-    #       ;;
-    #   esac
-    # '';
+    sshScript = pkgs.writeScriptBin "ssh-hook" ''
+      #!${pkgs.bash}/bin/bash
+      set -eu
+      set -o pipefail
+      [[ -z "''${SSH_ORIGINAL_COMMAND:-}" ]] && exit 1
+      case "''${SSH_ORIGINAL_COMMAND}" in
+        "hook"*)
+          exec sudo $out/bin/hook
+          ;;
+        *)
+          echo "invalid command"
+          exit 1
+          ;;
+      esac
+    '';
 
   in {
 
     services.openssh.enable = true;
 
-    # services.openssh.extraConfig = ''
-    #   Match User hook
-    #     PasswordAuthentication no
-    #     AllowAgentForwarding no
-    #     AllowTcpForwarding no
-    #     X11Forwarding no
-    #     ForceCommand <module>/bin/hook
-    # '';
+    services.openssh.extraConfig = ''
+      Match User hook
+        PasswordAuthentication no
+        AllowAgentForwarding no
+        AllowTcpForwarding no
+        X11Forwarding no
+        ForceCommand $out/bin/ssh-hook
+    '';
 
-      users.users.hook = {
-        isNormalUser = true;
-      };
+    users.users.hook = {
+      isNormalUser = true;
+    };
 
-      security.sudo = {
-        enable = true;
-        extraRules = [{
-          commands = [
-            {
-              command = "/run/current-system/sw/bin/whoami";
-              options = [ "NOPASSWD" ];
-            }
-            #{
-            #  command = "nixos-rebuild build --flake .#utm";
-            #  options = [ "NOPASSWD" ];
-            #}
-          ];
-          groups = [ "wheel" ]; # FIXME only the user "foo"
-        }];
-      };
+    security.sudo = {
+      enable = true;
+      extraRules = [{
+        commands = [
+          {
+            command = "$out/bin/hook";
+            options = [ "NOPASSWD" ];
+          }
+        ];
+        groups = [ "wheel" ]; # FIXME only the user "foo"
+      }];
+    };
 
 }
