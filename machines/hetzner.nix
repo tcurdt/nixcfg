5ab2a71947ffb864aae49d6c39fe764b22a0827f
Torsten Curdt <tcurdt@vafer.org>
2023-12-10 02:06:51 +0100

    docker, podman, k3s

--- machines/hetzner.nix
+++ machines/hetzner.nix
@@ -1,15 +1,3 @@
-# #{ config, nodes, self, ... }:
-# #{ config, pkgs, ... }:
-# { nixpkgs, ... }:
-# {
-#   # nixpkgs.lib.nixosSystem {
-#     networking.hostName = "nixos";
-#     networking.domain = "utm";
-#     #system.stateVersion = "23.05";
-#     system = "aarch64-linux";
-#   # };
-# }
-
 { nixpkgs, ... }: let
 
   hardware = "hetzner";
