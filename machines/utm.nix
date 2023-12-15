56c491f57aea2c82db19ca782bdc71e6c8ed28ed
Torsten Curdt <tcurdt@vafer.org>
2023-12-16 00:46:27 +0100

    more testing

--- machines/utm.nix
+++ machines/utm.nix
@@ -4,6 +4,8 @@
   hostPlatform = "aarch64-linux";
   hostName = "nixos";
 
+  pkgs = nixpkgs.legacyPackages.${hostPlatform};
+
 in nixpkgs.lib.nixosSystem {
 
   modules = [
@@ -31,6 +33,7 @@ in nixpkgs.lib.nixosSystem {
         virtualHosts."whoami.vafer.work" = {
           extraConfig = ''
             reverse_proxy http://127.0.0.1:8080
+            tls internal
           '';
         };
       };
@@ -49,6 +52,83 @@ in nixpkgs.lib.nixosSystem {
           # };
           # extraOptions = [ "--pod=foo" ];
         };
+      };
+    }
+
+    {
+      services.postgresql = {
+        enable = true;
+        ensureDatabases = [
+          "foo"
+        ];
+        authentication = pkgs.lib.mkOverride 10 ''
+          #type database  DBuser  auth-method
+          local all       all     trust
+        '';
+        # enableTCPIP = true;
+      };
+    }
+
+    {
+      services.mysql = {
+        enable = true;
+        package = pkgs.mariadb;
+        settings.mysqld = {
+          bind-address = "localhost";
+        };
+        # bind = "localhost";
+        ensureDatabases = [
+          "foo"
+        ];
+        initialScript = pkgs.writeText "setup.sql" ''
+          CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED WITH mysql_native_password;
+          ALTER USER 'root'@'localhost' IDENTIFIED BY 'secret';
+          GRANT ALL PRIVILEGES ON foo.* TO 'root'@'localhost';
+        '';
+        # initialScript = nixpkgs.writeText "setup.sql" ''
+        #       CREATE DATABASE IF NOT EXISTS `foo`;
+        #       CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED WITH mysql_native_password;
+        #       ALTER USER 'root'@'localhost' IDENTIFIED BY 'secret';
+        #       GRANT ALL PRIVILEGES ON foo.* TO 'root'@'localhost';
+        # '';
+        # ensureUsers = [
+        #   {
+        #     name = "root";
+        #     ensurePermissions = {
+        #       "foo.*" = "ALL PRIVILEGES";
+        #     };
+        #   }
+        # ];
+        # [
+        #   {
+        #     name = "nextcloud";
+        #     ensurePermissions = {
+        #       "nextcloud.*" = "ALL PRIVILEGES";
+        #     };
+        #   }
+        #   {
+        #     name = "backup";
+        #     ensurePermissions = {
+        #       "*.*" = "SELECT, LOCK TABLES";
+        #     };
+        #   }
+        # ]
+        # initialScript = nixpkgs.writeText "setup.sql" ''
+        #   GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+        #   CREATE DATABASE foo;
+        #   CREATE TABLE identity.ident (name VARCHAR(25) NOT NULL);
+        #   INSERT INTO identity.ident (name) VALUES ("${target.name}");
+        #         GRANT ALL PRIVILEGES ON *.* TO 'myapp'@'10.50.2.%' IDENTIFIED BY "password" WITH GRANT OPTION;
+        #         GRANT ALL PRIVILEGES ON *.* TO 'repl'@'10.50.2.%' IDENTIFIED BY "password" WITH GRANT OPTION;
+        #         DROP DATABASE IF EXISTS myapp;
+        #         CREATE DATABASE myapp;
+        # '';
+        # initialScript = nixpkgs.writeText "setup.sql" ''
+        #       CREATE DATABASE IF NOT EXISTS `example`;
+        #       CREATE USER IF NOT EXISTS 'laravel'@'webserver' IDENTIFIED WITH mysql_native_password;
+        #       ALTER USER 'laravel'@'webserver' IDENTIFIED BY '${databasePassword}';
+        #       GRANT ALL PRIVILEGES ON example.* TO 'laravel'@'webserver';
+        # '';
       };
     }
 
