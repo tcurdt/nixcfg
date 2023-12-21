72a0e3b744772f5da990a5796c236ad4d937267e
Torsten Curdt <tcurdt@vafer.org>
2023-12-21 03:15:15 +0100

    trying refactor

---
+++ modules/backup.nix
@@ -0,0 +1,55 @@
+
+{ config, pkgs, inputs, ... }:
+{
+
+  systemd.user.services.restore = {
+    enable = true;
+
+    description = "restore missing databases";
+
+    startAt = "minutely";
+
+    # serviceConfig = {
+    #   ExecStart = "${pkgs.pnmixer}/bin/pnmixer";
+    # };
+
+    requires = [ "postgres" ];
+    after = [ "postgres" ];
+  };
+
+
+
+  # systemd.user.services.backup = {
+  #   enable = true;
+
+  #   description = "backup databases";
+
+  #   startAt = "daily";
+
+
+  #   wantedBy = ["multi-user.target"];
+  # };
+
+  # systemd.tmpfiles.rules = [
+  #   "d /var/www"
+  #   "d /var/www/example.org 0750 example example"
+  # ];
+
+  # unitConfig = {
+  #   ConditionPathExists = "/var/www/example.org/.env";
+  #   ConditionDirectoryNotEmpty = "/var/www/example.org/vendor";
+  # };
+
+  # serviceConfig = {
+  #   ExecStart = "${pkgs.pnmixer}/bin/pnmixer";
+  # };
+  # serviceConfig = {
+  #   Type = "oneshot";
+  #   User = "example";
+  #   Group = "example";
+  #   SyslogIdentifier = "example-laravel-scheduler";
+  #   WorkingDirectory = "/var/www/example.org";
+  #   ExecStart = "${php'}/bin/php artisan schedule:run -v";
+  # };
+
+}
