faf4dfb23aba6a44b302d6ca36e572520138ac47
Torsten Curdt <tcurdt@vafer.org>
2024-01-10 05:04:35 +0100

    mongo

--- modules/db-mongo.nix
+++ modules/db-mongo.nix
@@ -1,27 +1,6 @@
 { config, pkgs, ... }:
 {
-  # sudo -u postgres psql
-  # \l
-  services.postgresql = {
+  services.mongodb = {
     enable = true;
-    # ensureDatabases = [
-    #   "bluesky"
-    # ];
-    ensureUsers = [{
-      name = "postgres";
-      # ensureDBOwnership = true;
-      # ensurePermissions = {
-      #   "bluesky.*" = "ALL PRIVILEGES";
-      # };
-    }];
-    # initialScript = pkgs.writeText "setup.sql" ''
-    #   ALTER USER postgres PASSWORD 'secret';
-    #   GRANT ALL PRIVILEGES ON DATABASE "bluesky" to postgres;
-    # '';
-    # authentication = pkgs.lib.mkOverride 10 ''
-    #   #type database  DBuser auth-method
-    #   local all       all    trust
-    # '';
-    # enableTCPIP = true;
   };
 }
