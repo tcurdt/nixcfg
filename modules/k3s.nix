fdeef48a30f10572f550bdfe1bfe046355bdd688
Torsten Curdt <tcurdt@vafer.org>
2024-01-16 01:43:32 +0100

    alias

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -1,15 +1,17 @@
 { config, pkgs, ... }:
 {
 
-  # so that pod can reach the API server
   # networking.firewall.allowedTCPPorts = [ 6443 ];
 
   services.k3s.enable = true;
   services.k3s.role = "server";
   services.k3s.extraFlags = toString [
-    # "--kubelet-arg=v=4" # Optionally add additional args to k3s
+    "--disable=traefik"
   ];
-  # --disable=traefik
+
+  environment.shellAliases = {
+    k9 = "k9s --kubeconfig /etc/rancher/k3s/k3s.yaml";
+  };
 
   environment.systemPackages = with pkgs; [
     k3s
