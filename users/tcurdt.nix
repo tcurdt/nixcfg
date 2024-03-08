ffa7525c7ed66c193393e8382637d7e6020549f6
Torsten Curdt <tcurdt@vafer.org>
2024-03-08 15:06:07 +0100

    home manager

--- users/tcurdt.nix
+++ users/tcurdt.nix
@@ -1,5 +1,11 @@
-{ config, pkgs, ... }:
+{ config, pkgs, inputs, ... }:
 {
+
+  imports = [ inputs.home-manager.nixosModules.default ];
+  # imports = [ home-manager.nixosModules.default ];
+  # imports = [ <home-manager/nixos> ];
+  # imports = [ (import "${home-manager}/nixos") ];
+
   users.users = {
     tcurdt = {
       isNormalUser = true;
@@ -15,4 +21,46 @@
       hashedPassword = "*"; # no password allowed
     };
   };
+
+  # programs.git = {
+  #   enable = true;
+  #   userName  = "tcurdt";
+  #   userEmail = "tcurdt@vafer.org";
+  # };
+
+  # home.packages = with pkgs; [
+  #   tmux
+  #   htop
+  # ];
+
+  # programs.home-manager.enable = true;
+
+  home-manager.users.tcurdt = { pkgs, ... }: {
+    home.sessionVariables = {
+      PAGER = "less";
+      EDITOR = "nano";
+      CLICOLOR = 1;
+    };
+    home.stateVersion = "23.11";
+  };
+
+  # home-manager.darwinModules.home-manager = {
+  #   home-manager = {
+  #     useGlobalPkgs = true;
+  #     useUserPkgs = true;
+  #     users.tcurdt = {
+  #       home.packages = with pkgs; [
+  #         ripgrep
+  #         curl
+  #         less
+  #       ];
+  #       home.sessionVariables = {
+  #         PAGER = "less";
+  #         EDITOR = "nano";
+  #         CLICOLOR = 1;
+  #       };
+  #     };
+  #   };
+  # };
+
 }
