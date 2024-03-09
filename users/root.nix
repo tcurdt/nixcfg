bc0d2d858865146e5685df4f5d1bfd18e4d31c27
Torsten Curdt <tcurdt@vafer.org>
2024-03-09 15:43:48 +0100

    cleanup

--- users/root.nix
+++ users/root.nix
@@ -3,14 +3,12 @@
 
   imports = [ inputs.home-manager.nixosModules.default ];
 
-  users.users = {
-    root = import ./default.nix // {
+  users.users.root = import ./default.nix // {
 
-      # password = "secret";
-      # promptInitialPassword = true;
-      # hashedPassword = "*"; # no password allowed
+    # password = "secret";
+    # promptInitialPassword = true;
+    # hashedPassword = "*"; # no password allowed
 
-    };
   };
 
   home-manager.users.root = import ../home/tcurdt.nix pkgs // {
--- users/tcurdt.nix
+++ users/tcurdt.nix
@@ -3,14 +3,13 @@
 
   imports = [ inputs.home-manager.nixosModules.default ];
 
-  users.users = {
-    tcurdt = import ./default.nix // {
+  users.users.tcurdt = import ./default.nix // {
 
-      isNormalUser = true;
-      extraGroups = [ "wheel" "docker" ];
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
 
-      hashedPassword = "*"; # no password allowed
-    };
+    hashedPassword = "*"; # no password allowed
+
   };
 
   home-manager.users.tcurdt = import ../home/tcurdt.nix pkgs // {
