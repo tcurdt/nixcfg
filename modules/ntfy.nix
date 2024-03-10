bc1365843610bda1cd6299587a1040752f5ca825
Torsten Curdt <tcurdt@vafer.org>
2024-03-10 12:31:14 +0100

    tighten down ntfy

--- modules/ntfy.nix
+++ modules/ntfy.nix
@@ -6,6 +6,9 @@
     settings = {
       base-url = "https://ntfy.vafer.org";
       listen-http = "127.0.0.1:8080";
+      auth-default-access = "deny-all";
+      behind-proxy = true;
+      enable-login = false;
     };
   };
 }
