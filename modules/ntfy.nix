bb465bbdbe4db794f54f1ca887c7da6a979f2b96
Torsten Curdt <tcurdt@vafer.org>
2024-03-11 00:53:06 +0000

    notification proxying

--- modules/ntfy.nix
+++ modules/ntfy.nix
@@ -5,6 +5,8 @@
 
     settings = {
       base-url = "https://ntfy.vafer.org";
+      upstream-base-url = "https://ntfy.sh";
+      # upstream-access-token:
       listen-http = "127.0.0.1:8080";
       auth-default-access = "deny-all";
       behind-proxy = true;
