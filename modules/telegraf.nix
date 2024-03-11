f669278013695b7d8587e624f8980ad05103a6f6
Torsten Curdt <tcurdt@vafer.org>
2024-03-11 13:29:13 +0000

    fixed telegraf config

--- modules/telegraf.nix
+++ modules/telegraf.nix
@@ -25,60 +25,60 @@
         #     "internal_agent"
         #   ];
         # };
-        cpu = {};
+        cpu = [{}];
         # linux_cpu = {};
-        mem = {};
-        swap = {};
-        kernel = {};
-        system = {};
-        # sysstat = {};
-        processes = {};
-        # procstat = {};
-        # interrupts = {};
-        # conntrack = {};
-        # net = {};
-        # netstat = {};
-        disk = {
+        mem = [{}];
+        swap = [{}];
+        kernel = [{}];
+        system = [{}];
+        # sysstat = [{}];
+        processes = [{}];
+        # procstat = [{}];
+        # interrupts = [{}];
+        # conntrack = [{}];
+        # net = [{}];
+        # netstat = [{}];
+        disk = [{
           mount_points = [
             "/"
           ];
-        };
-        diskio = {};
-        prometheus = {
+        }];
+        diskio = [{}];
+        prometheus = [{
           urls = [
-            "http://localhost:8086/metrics"
+            # "http://127.0.0.1:8086/metrics"
           ];
           # metric_version = 2;
           # fieldinclude = [
           #   "process_resident_memory_bytes"
           # ];
-        };
-        postgresql = {
-          ignored_databases = [
-            "postgres"
-            "template0"
-            "template1"
-          ];
-        };
-        redis = {};
-        # x509_cert = {
+        }];
+        #postgresql = [{
+        #  ignored_databases = [
+        #    "postgres"
+        #    "template0"
+        #    "template1"
+        #  ];
+        #}];
+        redis = [{}];
+        # x509_cert = [{
         #   sources = [
         #     tcp://api.vafer.org:443
         #     tcp://ntfy.vafer.org:443
         #   ];
-        # };
-        # fail2ban = {};
-        # mqtt_consumer = {
+        # }];
+        # fail2ban = [{}];
+        # mqtt_consumer = [{
         #   servers = ["tcp://foo:1883"];
         #   topics = ["sensor/"];
         #   username = "";
         #   password = "";
         #   data_format = "influx";
-        # };
-        # docker = {
+        # }];
+        # docker = [{
         #   endpoint = "unix:///var/run/docker.sock";
-        # };
-        # exec = {
+        # }];
+        # exec = [{
         #   commands = [
         #     "/tmp/test.sh"
         #     "/usr/bin/mycollector --foo=bar"
@@ -87,24 +87,24 @@
         #   environment = [];
         #   timeout = "5s";
         #   name_suffix = "_mycollector";
-        # };
+        # }];
       };
       outputs = {
-        # file = {
+        # file = [{
         #   files = [
         #     "/dev/null"
         #   ];
-        # };
-        influxdb_v2 = {
+        # }];
+        influxdb_v2 = [{
           urls = [
             "http://127.0.0.1:8086"
           ];
           organization = "\${TELEGRAF_INFLUX_ORG}";
           bucket = "\${TELEGRAF_INFLUX_BUCKET}";
           token = "\${TELEGRAF_INFLUX_TOKEN}";
-        };
+        }];
       };
     };
-    environmentFiles = [ /secrets/telegraf.env ];
+    environmentFiles = [ "/secrets/telegraf.env" ];
   };
 }
