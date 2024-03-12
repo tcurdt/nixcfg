f67a2e547e0a415797706fa0a828439317532a57
Torsten Curdt <tcurdt@vafer.org>
2024-03-12 19:23:02 +0000

    cleanup

--- modules/telegraf.nix
+++ modules/telegraf.nix
@@ -1,5 +1,6 @@
 { config, pkgs, ... }:
 {
+  # https://github.com/influxdata/telegraf/blob/master/docs/CONFIGURATION.md
   services.telegraf = {
     enable = true;
     extraConfig = {
@@ -9,17 +10,11 @@
       agent = {
         interval = "30s";
         hostname = "\${TELEGRAF_INFLUX_HOST}";
-        # round_interval = true
-        # metric_batch_size = 1000
-        # metric_buffer_limit = 10000
-        # collection_jitter = "0s"
-        # flush_interval = "10s"
-        # flush_jitter = "0s"
-        # precision = ""
-        # omit_hostname = false
       };
       inputs = {
-        cpu = [{ taginclude = { cpu = [ "cpu-total" ]; }; }];
+        cpu = [{
+          taginclude = { cpu = [ "cpu-total" ]; };
+        }];
         # linux_cpu = {};
         mem = [{}];
         # swap = [{}]; # covered in mem
@@ -30,60 +25,49 @@
         # procstat = [{}];
         # interrupts = [{}];
         conntrack = [{}];
-        net = [{ fieldexclude = [ "icmp*" "tcp*" "udp*" "ip*" ]; }];
-        netstat = [{}];
-        disk = [{
-          mount_points = [
-            "/"
-          ];
+        net = [{
+          fieldexclude = [ "icmp*" "tcp*" "udp*" "ip*" ];
         }];
+        netstat = [{}];
+        disk = [{ mount_points = [ "/" ]; }];
         diskio = [{}];
         prometheus = [{
-          urls = [
-            "http://127.0.0.1:2019/metrics" # caddy
-          ];
+          urls = [ "http://127.0.0.1:2019/metrics" ]; # caddy
           tags = { service = "caddy"; };
           metric_version = 2;
-          fieldinclude = [
-            "caddy_*"
-            "process_*"
-          ];
+          fieldinclude = [ "caddy_*" "process_*" ];
         }{
-          urls = [
-            "http://127.0.0.1:8086/metrics" # influxdb
-          ];
+          urls = [ "http://127.0.0.1:8086/metrics" ]; # influxdb
           tags = { service = "influxdb"; };
           metric_version = 2;
-          fieldinclude = [
-            "boltdb_*"
-            "storage_*"
-          ];
+          fieldinclude = [ "boltdb_*" "storage_*" ];
         }];
-        # fail2ban = [{}]; # needs sudo configuration
-        #postgresql = [{
-        #  address = "host=localhost user=postgres sslmode=disable";
-        #  ignored_databases = [
-        #    "postgres"
-        #    "template0"
-        #    "template1"
-        #  ];
-        #}];
+        # fail2ban = [{ # needs sudo configuration
+        #   interval = 
+        # }];
+        # postgresql = [{
+        #   interval = 
+        #   address = "host=localhost user=postgres sslmode=disable";
+        #   ignored_databases = [
+        #     "postgres"
+        #     "template0"
+        #     "template1"
+        #   ];
+        # }];
         redis = [{
           servers = [ "tcp://127.0.0.1:6379" ];
-          fieldinclude = [
-            "keyspace_*"
-            "used_*"
-          ];
+          fieldinclude = [ "keyspace_*" "used_*" "tracking_*" "io_threaed_*" ];
         }];
         # x509_cert = [{
+        #   interval = 
         #   sources = [
         #     tcp://api.vafer.org:443
         #     tcp://ntfy.vafer.org:443
         #   ];
         # }];
         # mqtt_consumer = [{
-        #   servers = ["tcp://foo:1883"];
-        #   topics = ["sensor/"];
+        #   servers = [ "tcp://foo:1883" ];
+        #   topics = [ "sensor/" ];
         #   username = "";
         #   password = "";
         #   data_format = "influx";
@@ -92,6 +76,7 @@
         #   endpoint = "unix:///var/run/docker.sock";
         # }];
         # exec = [{
+        #   interval = 
         #   commands = [
         #     "/tmp/test.sh"
         #     "/usr/bin/mycollector --foo=bar"
@@ -104,14 +89,10 @@
       };
       outputs = {
         # file = [{
-        #   files = [
-        #     "/dev/null"
-        #   ];
+        #   files = [ "/dev/null" ];
         # }];
         influxdb_v2 = [{
-          urls = [
-            "http://127.0.0.1:8086"
-          ];
+          urls = [ "http://127.0.0.1:8086" ];
           organization = "\${TELEGRAF_INFLUX_ORG}";
           bucket = "\${TELEGRAF_INFLUX_BUCKET}";
           token = "\${TELEGRAF_INFLUX_TOKEN}";
