c8185a3144dcd5e0da0b56ecad6cd3dd3f57bb33
Torsten Curdt <tcurdt@vafer.org>
2024-03-19 22:39:02 +0100

    cleanup

--- machines/home-goe.nix
+++ machines/home-goe.nix
@@ -10,7 +10,13 @@ in nixpkgs.lib.nixosSystem {
 
     # inputs.nixos-generators.nixosModules.all-formats
 
-    # inputs.release-go.nixosModules.default
+    {
+      nixpkgs.hostPlatform = hostPlatform;
+      networking.hostName = hostName;
+      networking.domain = "home";
+      system.stateVersion = "23.11";
+      # system.stateVersion = "24.05";
+    }
 
     ../hardware/home.nix
     disko.nixosModules.disko
@@ -42,30 +48,24 @@ in nixpkgs.lib.nixosSystem {
       };
     }
 
+    ../users/root.nix
+    ../users/tcurdt.nix
+
+    {
+      users.users.root.password = "secret";
+    }
+
     ../modules/server.nix
     ../modules/users.nix
 
     # ../modules/telegraf.nix
-    ../modules/db-influx.nix
-    ../modules/homeassistant.nix
+    # ../modules/db-influx.nix
+    # ../modules/homeassistant.nix
 
-    ../modules/zerotierone.nix
+    # ../modules/zerotierone.nix
     # ../modules/tailscale.nix
 
-    {
-      nixpkgs.hostPlatform = hostPlatform;
-      networking.hostName = hostName;
-      networking.domain = "home";
-      system.stateVersion = "23.11";
-      # system.stateVersion = "24.05";
-    }
-
-    ../users/root.nix
-    ../users/tcurdt.nix
-
-    # {
-    #   users.users.root.password = "secret";
-    # }
+    # inputs.release-go.nixosModules.default
 
     {
       networking.firewall.allowedTCPPorts = [ 80 443 ];
@@ -74,12 +74,12 @@ in nixpkgs.lib.nixosSystem {
 
         # curl -k --resolve ntfy.vafer.org:443:127.0.0.1 https://ntfy.vafer.org
 
-        virtualHosts."homeassistant.home" = {
-          extraConfig = ''
-            reverse_proxy 127.0.0.1:2020
-            tls internal
-          '';
-        };
+        # virtualHosts."homeassistant.home" = {
+        #   extraConfig = ''
+        #     reverse_proxy 127.0.0.1:2020
+        #     tls internal
+        #   '';
+        # };
 
       };
     }
