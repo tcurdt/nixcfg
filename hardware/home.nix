fc59b0213a2b3b23eba146191397f920015615f1
Torsten Curdt <tcurdt@vafer.org>
2024-03-19 23:03:10 +0100

    grub

--- hardware/home.nix
+++ hardware/home.nix
@@ -10,6 +10,10 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
+  boot.loader.grub.enable = true;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.efiInstallAsRemovable = true;
+
   # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
   # (the default) this is the recommended approach. When using systemd-networkd it's
   # still possible to use this option, but it's recommended to use it in conjunction
