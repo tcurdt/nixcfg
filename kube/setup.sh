3f8975b09ca29c1d727a4ada535ae7f7056f9059
Torsten Curdt <tcurdt@vafer.org>
2024-05-08 16:56:43 +0200

    postgres superuser

--- kube/setup.sh
+++ kube/setup.sh
@@ -7,17 +7,23 @@ kubectl create secret docker-registry github \
 --docker-password=$GITHUB_TOKEN \
 --docker-email=tcurdt@vafer.org
 
-kubectl create secret generic postgres-secret \
+kubectl create secret generic postgres-superuser \
 --from-literal=username=postgres \
 --from-literal=password=secret
 
-# kubectl create secret generic postgres-secret \
+# kubectl create secret generic postgres \
 # --from-file=username=./username.txt \
 # --from-file=password=./password.txt
 
-kubectl apply -f mysql-*
-kubectl apply -f postgres-*
+kubectl apply \
+ -f postgres-volume.yaml \
+ -f postgres-deployment.yaml \
+ -f postgres-service.yaml
 
+
+kubectl apply -f mysql-*
+
+kubectl exec -it pod/postgres-5f8c6bcbc8-n7s9v -- psql -U postgres
 # kubectl exec -it postgres-665b7554dc-cddgq -- pg_dump -U ps_user -d ps_db > db_backup.sql
 # kubectl cp db_backup.sql postgres-665b7554dc-cddgq:/tmp/db_backup.sql
 # kubectl exec -it postgres-665b7554dc-cddgq -- /bin/bash
