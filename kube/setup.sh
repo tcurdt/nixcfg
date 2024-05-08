398762e9c82a40316ee819d8e9a94a95f792667d
Torsten Curdt <tcurdt@vafer.org>
2024-05-08 19:07:37 +0200

    valkey

--- kube/setup.sh
+++ kube/setup.sh
@@ -23,13 +23,19 @@ kubectl apply \
  -f mysql-deployment.yaml \
  -f mysql-service.yaml
 
-# kubectl exec -it pod/postgres-5f8c6bcbc8-n7s9v -- psql -U postgres
-# kubectl exec -it pod/mysql-5f8c6bcbc8-n7s9v -- mysql -U root
+kubectl apply \
+ -f valkey-deployment.yaml \
+ -f valkey-service.yaml
 
-# kubectl exec -it postgres-665b7554dc-cddgq -- pg_dump -U ps_user -d ps_db > db_backup.sql
+# kubectl exec -it pod/postgres-5f8c6bcbc8-n7s9v -- psql -U postgres
+# kubectl exec -it pod/mysql-5f8c6bcbc8-n7s9v -- mysql -u root -p
+
+# kubectl exec -it postgres-665b7554dc-cddgq -- pg_dump -U postgres -d database > db_backup.sql
+
 # kubectl cp db_backup.sql postgres-665b7554dc-cddgq:/tmp/db_backup.sql
 # kubectl exec -it postgres-665b7554dc-cddgq -- /bin/bash
 # psql -U ps_user -d ps_db -f /tmp/db_backup.sql
+
 # kubectl create secret generic postgres-superuser \
 # --from-file=username=./username.txt \
 # --from-file=password=./password.txt
