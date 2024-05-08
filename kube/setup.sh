406bd2c0076989756f40692e11bd8a432edc069c
Torsten Curdt <tcurdt@vafer.org>
2024-05-08 17:15:35 +0200

    mysql

--- kube/setup.sh
+++ kube/setup.sh
@@ -7,24 +7,29 @@ kubectl create secret docker-registry github \
 --docker-password=$GITHUB_TOKEN \
 --docker-email=tcurdt@vafer.org
 
-kubectl create secret generic postgres-superuser \
---from-literal=username=postgres \
+kubectl create secret generic mysql-superuser \
 --from-literal=password=secret
 
-# kubectl create secret generic postgres \
-# --from-file=username=./username.txt \
-# --from-file=password=./password.txt
+kubectl create secret generic postgres-superuser \
+--from-literal=password=secret
 
 kubectl apply \
  -f postgres-volume.yaml \
  -f postgres-deployment.yaml \
  -f postgres-service.yaml
 
+kubectl apply \
+ -f mysql-volume.yaml \
+ -f mysql-deployment.yaml \
+ -f mysql-service.yaml
 
-kubectl apply -f mysql-*
+# kubectl exec -it pod/postgres-5f8c6bcbc8-n7s9v -- psql -U postgres
+# kubectl exec -it pod/mysql-5f8c6bcbc8-n7s9v -- mysql -U root
 
-kubectl exec -it pod/postgres-5f8c6bcbc8-n7s9v -- psql -U postgres
 # kubectl exec -it postgres-665b7554dc-cddgq -- pg_dump -U ps_user -d ps_db > db_backup.sql
 # kubectl cp db_backup.sql postgres-665b7554dc-cddgq:/tmp/db_backup.sql
 # kubectl exec -it postgres-665b7554dc-cddgq -- /bin/bash
 # psql -U ps_user -d ps_db -f /tmp/db_backup.sql
+# kubectl create secret generic postgres-superuser \
+# --from-file=username=./username.txt \
+# --from-file=password=./password.txt
