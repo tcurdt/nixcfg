1111f84915120fc04e085abf23c853f7969416d0
Torsten Curdt <tcurdt@vafer.org>
2024-05-09 16:19:57 +0000

    hosts

---
+++ kube/hosts.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+
+set -e
+
+lookup() {
+    service_name=$1
+    coredns_ip=$2
+
+    result=$(dig +short @$coredns_ip $service_name)
+
+    if [ -n "$result" ]; then
+        echo "$result"
+    else
+        echo "failed to find ip for $service_name"
+        exit 1
+    fi
+}
+
+create_hosts_file() {
+    coredns_ip="10.43.0.10"
+    hosts_new="/etc/hosts.new"
+
+    echo1=$(lookup "echo1.default.svc.cluster.local" $coredns_ip)
+
+    echo "" | tee $hosts_new
+
+    echo "$echo1 echo1.service" | tee -a $hosts_new
+
+}
+
+create_hosts_file
