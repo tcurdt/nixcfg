7ca8c67e995daceaab9ced0f5a966a125b90d5dd
Torsten Curdt <tcurdt@vafer.org>
2024-05-18 21:57:45 +0200

    more docs

--- modules/k3s.nix
+++ modules/k3s.nix
@@ -1,19 +1,31 @@
 { config, pkgs, ... }:
 {
 
-  # networking.firewall.allowedTCPPorts = [ 6443 ];
+  # networking.firewall.allowedTCPPorts = [
+  #   6443 # k3s: required so that pods can reach the API server (running on port 6443 by default)
+  #   2379 # k3s, etcd clients: required if using a "High Availability Embedded etcd" configuration
+  #   2380 # k3s, etcd peers: required if using a "High Availability Embedded etcd" configuration
+  # ];
+  # networking.firewall.allowedUDPPorts = [
+  #   # 8472 # k3s, flannel: required if using multi-node for inter-node networking
+  # ];
 
-  services.k3s.enable = true;
-  services.k3s.role = "server";
-  services.k3s.extraFlags = toString [
-    "--disable=traefik"
-    "--disable=metrics-server"
-    # "--disable=servicelb"
-    # "--disable-cloud-controller"
-    # "--disable-kube-proxy"
-    # "--disable-network-policy"
-    # "--disable-helm-controller"
-  ];
+  services.k3s = {
+    enable = true;
+    role = "server";
+    # token = "secret";
+    # clusterInit = true;
+    # serverAddr = "https://<ip of first node>:6443";
+    extraFlags = toString [
+      "--disable=traefik"
+      "--disable=metrics-server"
+      # "--disable=servicelb"
+      # "--disable-cloud-controller"
+      # "--disable-kube-proxy"
+      # "--disable-network-policy"
+      # "--disable-helm-controller"
+    ];
+  };
 
   networking.nameservers = [ "10.43.0.10" ];
 
