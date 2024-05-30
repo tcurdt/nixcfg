f467e45845aab9cd70816614eaa0f28e72fe4c5c
Torsten Curdt <tcurdt@vafer.org>
2024-05-30 22:55:01 +0200

    wip

--- kube/setup.sh
+++ kube/setup.sh
@@ -5,9 +5,11 @@ mkdir -p /srv/volumes/grafana && chown 65534:65534 /srv/volumes/grafana
 mkdir -p /srv/volumes/postgres && chown 65534:65534 /srv/volumes/postgres
 mkdir -p /srv/volumes/prometheus && chown 65534:65534 /srv/volumes/prometheus
 
+kubectl delete secret postgres-superuser -n infra
 kubectl create secret generic postgres-superuser \
 --from-literal=username=postgres \
 --from-literal=password=secret \
+--from-literal=url=postgres://postgres:secret@postgres:5432/postgres \
 -n infra
 
 # kubectl create secret generic postgres-superuser \
