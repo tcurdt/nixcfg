fde0fbe7c892bcad39c8e6a59dc164bd65d86556
Torsten Curdt <tcurdt@vafer.org>
2024-06-01 17:38:04 +0200

    cleanup

--- kube/infra/deploy.sh
+++ kube/infra/deploy.sh
@@ -1,28 +1,3 @@
-mkdir -p /srv/volumes/cdn && chown 65534:65534 /srv/volumes/cdn
-mkdir -p /srv/volumes/loki && chown 10001:10001 /srv/volumes/loki
-mkdir -p /srv/volumes/mysql && chown 65534:65534 /srv/volumes/mysql
-mkdir -p /srv/volumes/thanos && chown 65534:65534 /srv/volumes/thanos
-mkdir -p /srv/volumes/grafana && chown 65534:65534 /srv/volumes/grafana
-mkdir -p /srv/volumes/postgres && chown 65534:65534 /srv/volumes/postgres
-mkdir -p /srv/volumes/prometheus && chown 65534:65534 /srv/volumes/prometheus
-
-kubectl apply \
- -f namespaces.yaml
-
-kubectl delete secret postgres-superuser -n infra
-kubectl create secret generic postgres-superuser \
---from-literal=username=postgres \
---from-literal=password=secret \
---from-literal=url=postgres://postgres:secret@postgres:5432/postgres \
--n infra
-
-kubectl delete secret mysql-superuser -n infra
-kubectl create secret generic mysql-superuser \
---from-literal=username=root \
---from-literal=password=secret \
---from-literal=url=mysql://root:secret@mysql:3306/mysql \
--n infra
-
 # export GITHUB_USER=tcurdt
 # export GITHUB_TOKEN=...
 
@@ -39,15 +14,6 @@ kubectl create secret generic mysql-superuser \
 # --docker-password=$GITHUB_TOKEN \
 # --docker-email=tcurdt@vafer.org
 # -n test
-
-kubectl apply \
- -f infra
-
-kubectl apply \
- -f backend-test.yaml
-
-kubectl apply \
- -f backend-live.yaml
 
 
 cat <<EOF > kustomization.yaml
---
+++ kube/infra/setup.sh
@@ -0,0 +1,25 @@
+mkdir -p /srv/volumes/cdn && chown 65534:65534 /srv/volumes/cdn
+mkdir -p /srv/volumes/loki && chown 10001:10001 /srv/volumes/loki
+mkdir -p /srv/volumes/mysql && chown 65534:65534 /srv/volumes/mysql
+mkdir -p /srv/volumes/thanos && chown 65534:65534 /srv/volumes/thanos
+mkdir -p /srv/volumes/grafana && chown 65534:65534 /srv/volumes/grafana
+mkdir -p /srv/volumes/postgres && chown 65534:65534 /srv/volumes/postgres
+mkdir -p /srv/volumes/prometheus && chown 65534:65534 /srv/volumes/prometheus
+
+kubectl apply -f namespaces.yaml
+
+kubectl delete secret postgres-superuser -n infra
+kubectl create secret generic postgres-superuser \
+--from-literal=username=postgres \
+--from-literal=password=secret \
+--from-literal=url=postgres://postgres:secret@postgres:5432/postgres \
+-n infra
+
+kubectl delete secret mysql-superuser -n infra
+kubectl create secret generic mysql-superuser \
+--from-literal=username=root \
+--from-literal=password=secret \
+--from-literal=url=mysql://root:secret@mysql:3306/mysql \
+-n infra
+
+kubectl apply -f infra
