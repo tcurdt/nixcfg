31ae7f2e7b0d7128cf0d7bdf57d448eff00def32
Torsten Curdt <tcurdt@vafer.org>
2024-06-04 14:37:15 +0200

    deploy and backup

--- kube/infra/setup.sh
+++ kube/infra/setup.sh
@@ -1,3 +1,4 @@
+mkdir -p /srv/volumes/infra && chown 65534:101 /srv/volumes/infra
 mkdir -p /srv/volumes/cdn && chown 65534:65534 /srv/volumes/cdn
 mkdir -p /srv/volumes/loki && chown 10001:10001 /srv/volumes/loki
 mkdir -p /srv/volumes/mysql && chown 65534:65534 /srv/volumes/mysql
@@ -7,6 +8,13 @@ mkdir -p /srv/volumes/postgres && chown 65534:65534 /srv/volumes/postgres
 mkdir -p /srv/volumes/prometheus && chown 65534:65534 /srv/volumes/prometheus
 
 kubectl apply -f namespaces.yaml
+
+kubectl delete secret git-repo-infra -n infra
+kubectl create secret generic git-repo-infra \
+--from-literal=username= \
+--from-literal=password= \
+--from-literal=url=https://github.com/tcurdt/nixcfg.git \
+-n infra
 
 kubectl delete secret postgres-superuser -n infra
 kubectl create secret generic postgres-superuser \
