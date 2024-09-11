4893db48de53d4ef20bc32a71e3f6d6f81db347c
Torsten Curdt <tcurdt@vafer.org>
2024-09-11 22:25:58 +0200

    darwin and colmea

--- flake.nix
+++ flake.nix
@@ -6,8 +6,8 @@
     nixpkgs-stable.url = "github:nixos/nixpkgs/nixos-24.05";
     # nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
 
-    # darwin.url = "github:LnL7/nix-darwin";
-    # darwin.inputs.nixpkgs.follows = "nixpkgs-stable";
+    darwin.url = "github:LnL7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs-stable";
 
     home-manager.url = "github:nix-community/home-manager/release-24.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs-stable";
@@ -17,6 +17,12 @@
 
     impermanence.url = "github:nix-community/impermanence";
 
+    # deploy-rs.url = "github:serokell/deploy-rs";
+    # deploy-rs.inputs.nixpkgs.follows = "nixpkgs-stable";
+
+    # nixos-generators.url = "github:nix-community/nixos-generators";
+    # nixos-generators.inputs.nixpkgs.follows = "nixpkgs-stable";
+
     # agenix.url = "github:ryantm/agenix";
     # agenix.inputs.nixpkgs.follows = "nixpkgs-stable";
     # agenix.inputs.darwin.follows = "";
@@ -25,12 +31,6 @@
     # release-go.inputs.nixpkgs.follows = "nixpkgs-stable";
     # sshhook.url = "git+file:///Users/tcurdt/Desktop/nix/flake-sshhook/";
 
-    # deploy-rs.url = "github:serokell/deploy-rs";
-    # deploy-rs.inputs.nixpkgs.follows = "nixpkgs-stable";
-
-    # nixos-generators.url = "github:nix-community/nixos-generators";
-    # nixos-generators.inputs.nixpkgs.follows = "nixpkgs-stable";
-
   };
 
   outputs =
@@ -38,20 +38,26 @@
     , nixpkgs-stable
     , home-manager
     , impermanence
+    , darwin
+    # , deploy-rs
     , ...
     } @ inputs:
 
     {
 
       # https://www.youtube.com/watch?v=LE5JR4JcvMg
-      # darwinConfigurations = {
-      #   shodan = import ./machines/shodan.nix inputs;
-      # };
+      darwinConfigurations = {
+        shodan = darwin.lib.darwinSystem {
+          specialArgs = { inherit inputs; };
+          modules = [ ./machines/shodan.nix ];
+          system = "aarch64-darwin";
+          # boot.binfmt.emulatedSystems = [ "aarch64-linux" "x86_64-linux" ];
+        };
+      };
 
       nixosConfigurations = {
 
         app = nixpkgs-stable.lib.nixosSystem {
-          # system = "x86_64-linux";
           specialArgs = {
             inherit inputs;
             # inputs = inputs // {
@@ -96,56 +102,104 @@
 
       };
 
-      # install-iso
-      # iso
-      # qcow
-      # qcow-efi
-      # amazon
-      # azure
-      # do
-      # gce
+      # boot.binfmt.emulatedSystems = [];
+      # deploy.nodes = {
 
-
-      # $ nix build .#packages.aarch64-linux.utm-iso
-      # packages.aarch64-linux.utm-iso = self.nixosConfigurations.utm.config.formats.iso;
-
-      # $ nix build .#packages.aarch64-linux.utm-iso
-      # error: a 'aarch64-linux' with features {} is required to build '/nix/store/srv0wy1ljxyiibv1alvn1pp9rgjs67xs-gomod2nix-symlink.drv', but I am a 'aarch64-darwin' with features {apple-virt, benchmark, big-parallel, nixos-test}
-      # 1. build the aarch64-linux image on aarch64-darwin
-      # 2. build a utm image instead of an iso
-
-      # $ nix-shell -p colmena
-      # colema = {
-      #   # meta.specialArgs.inputs = inputs;
-      #   # utm = import ./machines/utm.nix inputs;
-      #   meta = {};
-      #   utm = self.nixosConfigurations.utm // {
-      #     deployment = {
-      #       targetHost = "192.168.71.3";
-      #       targetUser = "root";
-      #       healthChecks = {
-      #         http = [
-      #           {
-      #             scheme = "http";
-      #             port = 80;
-      #             path = "/";
-      #             description = "check for http ingres";
-      #           }
-      #         ];
-      #       };
+      #   # nix run github:serokell/deploy-rs -- #app
+      #   app = {
+      #     hostname = "5.189.130.53";
+      #     remoteBuild = false;
+      #     # sshUser = "x";
+      #     profiles.system = {
+      #       user = "root";
+      #       path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.app;
       #     };
       #   };
+
+      #   # nix run github:serokell/deploy-rs -- #utm-arm
+      #   utm-arm = {
+      #     hostname = "192.168.71.3";
+      #     remoteBuild = false;
+      #     # sshUser = "x";
+      #     profiles.system = {
+      #       user = "root";
+      #       path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.utm-arm;
+      #     };
+      #   };
+
       # };
 
-      # $ nix run github:serokell/deploy-rs -- #utm
-      # deploy.nodes.utm = {
-      #   hostname = "192.168.71.3";
-      #   remoteBuild = true;
-      #   profiles.system = {
-      #     user = "root";
-      #     path = deploy-rs.lib.aarch64-linux.activate.nixos self.nixosConfigurations.utm;
-      #   };
-      # };
+      # nix-shell -p colmena
+      # colmena apply --on @vm
+      colema = {
+        # meta = {};
+        # meta.specialArgs.inputs = inputs;
+        # meta.specialArgs = { inherit inputs; };
+        # meta = {
+        #   nixpkgs = import nixpkgs {
+        #     system = "x86_64-linux";
+        #     overlays = [];
+        #   };
+        # };
+
+        utm-arm = self.nixosConfigurations.utm-arm // {
+          deployment = {
+            tags = [ "vm" ];
+            keys = {
+              foo = {
+                text = "";
+                # keyFile = "";
+                # keyCommand = [];
+                # user = "caddy"
+                # uploadAt = "post-activation";
+              };
+            };
+            targetHost = "192.168.71.3";
+            targetUser = "root";
+            # healthChecks = {
+            #   http = [
+            #     {
+            #       scheme = "http";
+            #       port = 80;
+            #       path = "/";
+            #       description = "check for http ingres";
+            #     }
+            #   ];
+            # };
+          };
+        };
+
+        utm-x86 = self.nixosConfigurations.utm-x86 // {
+          deployment = {
+            tags = [ "vm" ];
+            keys = {
+              foo = {
+                text = "";
+                # keyFile = "";
+                # keyCommand = [];
+                # user = "caddy"
+                # uploadAt = "post-activation";
+              };
+            };
+            targetHost = "192.168.71.3";
+            targetUser = "root";
+            # healthChecks = {
+            #   http = [
+            #     {
+            #       scheme = "http";
+            #       port = 80;
+            #       path = "/";
+            #       description = "check for http ingres";
+            #     }
+            #   ];
+            # };
+          };
+        };
+
+      };
+
+      # nix build .#packages.aarch64-linux.utm-iso
+      # packages.aarch64-linux.utm-iso = self.nixosConfigurations.utm.config.formats.iso;
 
     };
 }
--- machines/shodan.nix
+++ machines/shodan.nix
@@ -8,11 +8,22 @@
 
     {
       nix.extraOptions = ''
+        auto-optimise-store = true
         experimental-features = nix-command flakes
+        extra-platforms = x86_64-darwin aarch64-darwin
       '';
       services.nix-daemon.enable = true;
-      #nix.package = pkgs.nix;
+    }
 
+    # {
+    #   homebrew = {
+    #     enable = true;
+    #     casks = [
+    #     ];
+    #   };
+    # }
+
+    {
       # programs.zsh.enable = true;
       # programs.zsh.shellAliases = {
       #   ls = "ls -la";
