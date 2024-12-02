483f9fb0f0538ccaecaeff40b4f6020489a972a4
Torsten Curdt <tcurdt@vafer.org>
2024-12-02 12:17:13 +0100

    smtp in own module

--- modules/server.nix
+++ modules/server.nix
@@ -334,24 +334,6 @@
     '';
   };
 
-  # smtp
-
-  programs.msmtp = {
-    enable = true;
-    setSendmail = false;
-    accounts = {
-      default = {
-        auth = true;
-        tls = true;
-        # tls_starttls = false; # if sendmail hangs
-        from = "tcurdt@vafer.org";
-        host = "email-smtp.eu-central-1.amazonaws.com";
-        user = "AKIA3V6SV2TSVUAMXT4D";
-        passwordeval = "cat /secrets/msmtp.key";
-      };
-    };
-  };
-
   # https://github.com/maralorn/nix-output-monitor
   # system.activationScripts.diff = {
   #   supportsDryActivation = true;
---
+++ modules/smtp.nix
@@ -0,0 +1,22 @@
+{ pkgs, ... }:
+{
+
+  # smtp
+
+  programs.msmtp = {
+    enable = true;
+    setSendmail = false;
+    accounts = {
+      default = {
+        auth = true;
+        tls = true;
+        # tls_starttls = false; # if sendmail hangs
+        from = "tcurdt@vafer.org";
+        host = "email-smtp.eu-central-1.amazonaws.com";
+        user = "AKIA3V6SV2TSVUAMXT4D";
+        passwordeval = "cat /secrets/msmtp.key";
+      };
+    };
+  };
+
+}
